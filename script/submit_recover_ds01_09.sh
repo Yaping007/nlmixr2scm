be9b1ef4ac4a8e79f0c8de2fa97e9b90660c631e
YapingliuNovartis <yaping.liu_ext@novartis.com>
2026-07-02 22:51:59 +0200

    HPCE-sc16 ds01-09recoveryN=80

---
+++ script/submit_recover_ds01_09.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#BSUB -J "scm_recover_scn16[1-9]"
+#BSUB -q short
+#BSUB -n 4
+#BSUB -R "span[hosts=1]"
+#BSUB -M 2000
+#BSUB -W 00:45
+#BSUB -o logs/scm_recover_scn16_%J_%I.out
+#BSUB -e logs/scm_recover_scn16_%J_%I.err
+
+cd /home/liuya8j/nlmixr2scm/nlmixr2scm
+
+export PATH=/CHBS/apps/EB/software/R/4.5.1-gomklc-2023b-0.2/bin:$PATH
+export OMP_NUM_THREADS=2
+export MKL_NUM_THREADS=2
+export OPENBLAS_NUM_THREADS=2
+
+DS_ID=${LSB_JOBINDEX}
+echo "Running ds_id = $DS_ID on host $(hostname) at $(date)"
+
+Rscript script/PerformanceEvaluation_1DS_HPCE.R $DS_ID
+
+echo "Finished at $(date)"
